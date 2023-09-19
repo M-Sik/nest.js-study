8f8da74b326e31f9a1cb0759b0ce35aa84d28dc5
김명식 <rlaaudtlr233@naver.com>
2023-09-19 18:44:22 +0900

    컨트롤러 공부

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    console.log('fff');
+  @Get('hello/:id/:name')
+  getHello(@Req() req, @Body() body, @Param() param: { id: string; name: string }): string {
+    console.log(req);
+    console.log(param);
+    console.log(body);
     return this.appService.getHello();
   }
 }
