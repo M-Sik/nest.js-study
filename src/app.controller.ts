b9cb6299a269bca92a72fcc2df515e291f0836f3
김명식 <rlaaudtlr233@naver.com>
2023-09-19 18:59:41 +0900

    nest 모듈 생성 공부

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
-import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id/:name')
-  getHello(@Req() req, @Body() body, @Param() param: { id: string; name: string }): string {
-    console.log(req);
-    console.log(param);
-    console.log(body);
+  @Get()
+  getHello(): string {
     return this.appService.getHello();
   }
 }
