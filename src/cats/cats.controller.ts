676b2222099c63ce88f9dc82f65a529423b7c280
김명식 <rlaaudtlr233@naver.com>
2023-09-21 19:13:46 +0900

    [chore] 디렉터리 구조 변경 - 공통적으로 쓰이는 파일 common 폴더생성 후 이동

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -11,7 +11,7 @@ import {
   UseFilters,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
 
 @UseFilters(HttpExceptionFilter) // 클래스에 데코레이터를 사용하면 해당 컨트롤러에 전체 api 대한 요청에 대해서 HttpExceptionFilter에 전달됨
